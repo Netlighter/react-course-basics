59542513c4bf5a78f30f344a90b6063506250f19
Netlighter <jasoness@yandex.ru>
2021-03-24 19:43:24 +0300

    New Module: Components

---
+++ cra/src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+
+  // automatic native code
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default App;
