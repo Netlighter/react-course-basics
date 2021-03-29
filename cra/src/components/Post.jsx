02b6617c815de87da96ff3700c9eeefa18c9709f
Netlighter <jasoness@yandex.ru>
2021-03-29 20:36:48 +0300

    Add practice method

--- cra/src/components/Post.jsx
+++ cra/src/components/Post.jsx
@@ -1,5 +1,13 @@
-export function Post (props) {
-  const {name, cb} = props;
+export function Post (props) { // funcs used without state and lifecycle
+  const {name, cb, rm, id } = props;
   // makes it very pretty
-  return <h2 onClick={cb}>{name}</h2>
+  return (//don't forget when enter the line
+    <h2 onClick={cb}>
+      {name} 
+      <button onClick={() => rm(id)}>
+        delete!
+      </button>
+    </h2>
+  )
+    
 }
